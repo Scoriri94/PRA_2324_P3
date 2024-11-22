eb8c3b2374f46908a65ca8d96928f8dcd594009c
Sebastian <scoriri94>
2024-11-22 13:57:55 +0100

    Añadida la clase genérica Dict

---
+++ Dict.h
@@ -0,0 +1,16 @@
+#ifndef DICT_H
+#define DICT_H
+#include <string>
+
+template <typename V>
+
+class Dict {
+	public:
+		virtual void insert(const std::string& key, V value) = 0;
+		virtual V search(const std::string& key) = 0;
+		virtual V remove(const std::string& key) = 0;
+		virtual int entries() = 0;
+		virtual ~Dict() = default;
+};
+
+#endif
