375eff074747ac3a65ee45214a9642d2a593c672
scoriri94 <socriri@upv.edu.es>
2024-11-24 19:18:37 +0100

    Añadida implementacion BSNode.h

---
+++ BSnode.h
@@ -0,0 +1,22 @@
+#ifndef BSNODE_H
+#define BSNODE_H
+
+#include <ostream>
+
+template <typename T> 
+class BSNode {
+    public:
+        T elem;
+	BSNodez<T>* left;
+	BSNode<T>* right;
+	//metodos
+	BSNode(T elem, BSNode<T>* left = nullptr, BSNode<T>* right = nullptr) : elem(elem), left(nullptr), right(nullptr) {}
+
+	friend std::ostream& operator<<(std:ostream &out, const BSNode<T> &bsn) {
+		out << bsn.elem;
+		return out;
+	}
+    
+};
+
+#endif
